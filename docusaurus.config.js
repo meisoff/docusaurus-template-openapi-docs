8e6962963ab0dc79b01d995b4a50e8d297d11548
Alexander <78162257+meisoff@users.noreply.github.com>
2024-05-24 06:08:16 +0000

    workflows github

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "meisoff", // Usually your GitHub org/user name.
+  projectName: "docusaurus-template-openapi-docs", // Usually your repo name.
 
   presets: [
     [
